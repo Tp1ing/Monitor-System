3c61eef877948d120f49c800913d0ee622600af0
Tp1ing <87628895+Tp1ing@users.noreply.github.com>
2021-07-22 15:16:22 +0800

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pymysql
+# 打开数据库连接
+db = pymysql.connect(host="localhost",user="root",password="123177419",database="login" )
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+# 使用 execute() 方法执行 SQL，如果表存在则删除
+cursor.execute("DROP TABLE IF EXISTS admin")
+# 使用预处理语句创建表
+sql = """CREATE TABLE admin (
+         username  CHAR(20) NOT NULL,
+         password  CHAR(20) NOT NULL,
+         email  CHAR(20)  NOT NULL)"""
+cursor.execute(sql)
+# 关闭数据库连接
+db.close()
